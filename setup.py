86fd36ea73c238af0370a5282a3ded2dd02bc140
Denis Alexeev <github.com@alexeev.ws>
2021-11-13 17:37:43 +0300

    wrote README.md (#15)

--- setup.py
+++ setup.py
@@ -1,19 +1,55 @@
 from setuptools import setup
 
-setup(
-    version="0.0.1",
-    name="pytest_glamor_allure",
-    packages=["pytest_glamor_allure", "glamor"],
-    entry_points={
-        "pytest11": ["pytest_glamor_allure = pytest_glamor_allure.plugin"]
-    },
-    classifiers=["Framework :: Pytest"],
-    install_requires=[
-        "allure-pytest",
-        "allure-python-commons",
-        "pytest",
-        "attrs",
+with open('README.md') as readme:
+    long_description = readme.read()
+
+classifiers = (
+    [
+        'Framework :: Pytest',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Quality Assurance',
     ],
-    py_modules=["glamor", "pytest_glamor_allure", "pitest"],
-    python_requires=">=3.7",
 )
+
+install_requires = [
+    'allure-pytest',
+    'allure-python-commons',
+    'pytest',
+    'attrs',
+]
+
+entry_points = {
+    'pytest11': [
+        'pytest_glamor_allure = pytest_glamor_allure.plugin',
+    ],
+}
+
+if __name__ == '__main__':
+    setup(
+        name='pytest_glamor_allure',
+        version='0.0.1',
+        description='Extends allure-pytest functionality',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Denis Alexeev',
+        author_email='github.com@alexeev.ws',
+        maintainer='Denis Alexeev',
+        maintainer_email='github.com@alexeev.ws',
+        url='https://github.com/Denis-Alexeev/pytest-glamor-allure',
+        packages=['pytest_glamor_allure', 'glamor'],
+        py_modules=['glamor', 'pytest_glamor_allure', 'pitest'],
+        classifiers=classifiers,
+        licence='MIT',
+        install_requires=install_requires,
+        entry_points=entry_points,
+        python_requires='>=3.7',
+    )
